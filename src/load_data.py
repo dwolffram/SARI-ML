7465118f188813581b9336e6a3e87bbd11ccaf63
dwolffram <daniel-wolffram@gmx.de>
2024-12-20 14:57:57 +0100

    upadted several evaluation and plotting steps

--- src/load_data.py
+++ src/load_data.py
@@ -282,7 +282,7 @@ def add_truth(df, source='icosari', disease='sari', target=False):
     return df
 
 
-def load_predictions(period='post-covid', start='2023-12-14', end='2024-09-19', include_median=True, include_truth=True, target=True):
+def load_predictions(period='post-covid', start='2023-11-16', end='2024-09-19', include_median=True, include_truth=True, target=True):
     files = glob.glob(f'../data/{period}/submissions/**/*.csv', recursive=True)
     files = [f for f in files if '/old/' not in f]
     
